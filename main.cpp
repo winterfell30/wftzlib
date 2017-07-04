8b48fe1593e07f44c600340892a243b400ad15b3
winterfell30 <m17862917336@163.com>
2017-07-05 01:21:12 +0800

    for merge

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//ÓÃÓÚ²âÊÔ¿Õ¼äÅäÖÃÆ÷
+#include <time.h>
+#include "vector.h"
+#include <vector>
+#include <thread>
+
+void g() { std::cout << "Hello world" << std::endl; }
+
+#define print(x) std::cout << #x << "=" << x << std::endl;
+
+int main()
+{
+	clock_t start, finish;
+	start = clock();
+	mystl::vector<int> v(1);
+	v.push_back(2);
+	v.erase(v.begin());
+	print(*v.begin());
+	print(*(v.end() - 1));
+	print(v.size());
+	print(v.capacity());
+	for (mystl::vector<int>::iterator it = v.begin(); it != v.end(); it++) print(*it);
+	finish = clock();
+	std::cout << "time: " << (double)(finish - start) << std::endl;
+	return 0;
+}
