8dd933bdbb7103cc8b83d478414d720dfb06e52f
winterfell30 <m17862917336@163.com>
2019-07-01 00:13:06 +0800

    update binary dst

--- src/common/binary.h
+++ src/common/binary.h
@@ -5,19 +5,42 @@ class Binary
 {
 public:
     Binary() : _data(""), _size(0) {}
+
+    Binary(size_t size_) : _size(size_)
+    {
+        _data = new char[size_];
+    }
+
+    ~Binary() { delete []_data; _data = nullptr; }
     
-    Binary(const char* data_) : _data(data_), _size(strlen(data_)) {}
-
-    Binary(const char* data_, size_t size_) : _data(data_), _size(size_) {}
-
-    const char* data() const { return _data; }
+    char* data() const { return _data; }
 
     size_t size() const { return _size; }
+
+    size_t resize(size_t new_size, char val = char())
+    {
+        if (size() > new_size)
+        {
+            _size = new_size;
+        }
+        else
+        {
+            char* new_ptr = new char[new_size];
+            for (size_t i = 0; i < size(); i++)
+            {
+                new_ptr[i] = _data[i];
+            }
+            memset(new_ptr + size(), val, sizeof(char) * (new_size - size()));
+            delete []_data;
+            _data = new_ptr;
+            _size = new_size;
+        }
+    }
 
     char operator[](size_t n) const 
     {
         assert(n < size());
-         return _data[n]; 
+        return _data[n]; 
     }
 
     void clear()
@@ -27,6 +50,11 @@ public:
     }
 
 private:
+
+    Binary(char* data_) = delete;
+
+    Binary(char* data_, size_t size_) = delete;
+
     char* _data;
     size_t _size;
 };
--- src/common/bloomfilter.h
+++ src/common/bloomfilter.h
@@ -7,18 +7,26 @@ namespace wftz {
 class BloomFilter
 {
 public:
-    BloomFilter();
+    BloomFilter(const int32_t n_, const double false_positive_) :
+        _n(n_), _p(false_positive_), _m(0), _k(0), _bits()
+    {
+        _k = static_cast<int32_t>(-std::log(_p)  / std::log(2));
+        _m = static_cast<int32_t>(_k * n_ * 1.0 / std::log(2));
+        _bits.resize((_m + 7) / 8);
+    }
 
     virtual ~BloomFilter();
 
-    void add()
-    {
-    }
+    void insert()
 
     void check();
 
 private:
-    std::vector<Binary> _bits;
+    wftz::Binary _bits;
+    double _p;
+    int32_t _n;
+    int32_t _m;
+    int32_t _k;
 };
 
 }
